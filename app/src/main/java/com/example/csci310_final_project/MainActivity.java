40b5f7740b0b2d9372c9453c4a340af34edb3fe6
Yitian Yan <yitianya@usc.edu>
2022-10-28 11:21:30 -0700

    upload files

--- app/src/main/java/com/example/csci310_final_project/MainActivity.java
+++ app/src/main/java/com/example/csci310_final_project/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.csci310_final_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseDatabase root;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onClick(View view) {
+        root = FirebaseDatabase.getInstance();
+        reference = root.getReference("message");
+        //reference.setValue(inputText.getText().toString());
+    }
+
+
 }
