81686cd62c7e607e914accb0f0800e5688888aa0
Winnie Hou <hwinnie@usc.edu>
2022-10-29 01:05:24 -0700

    successful access to firestore database!

--- app/src/main/java/com/example/csci310_final_project/MatchActivity.java
+++ app/src/main/java/com/example/csci310_final_project/MatchActivity.java
@@ -15,6 +15,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -23,6 +24,9 @@ import com.google.firebase.firestore.QuerySnapshot;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MatchActivity extends AppCompatActivity {
     int myUserId = 0;
     @Override
@@ -30,83 +34,46 @@ public class MatchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
 
-        LinearLayout linearLayout = (LinearLayout) this.findViewById(R.id.linearlayout02);
-        TextView user = new TextView(this);
-        user.setText("user");
-        Button button = new Button(this);
-        button.setText("match!");
-        linearLayout.addView(user);
-        linearLayout.addView(button);
-        // init();
+        init();
     }
-    /*
     private void init(){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Map<String, String> all_acceptances = new HashMap<String, String>();
+        LinearLayout linearLayout = (LinearLayout) this.findViewById(R.id.linearlayout02);
+
+        String userName = "Winnie"; //TODO: get userName of current user
         db.collection("invitation")
+                .whereEqualTo("username", userName)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                //View view= LayoutInflater.from(getContext()).inflate(R.layout.post_layout,null);
-                                LinearLayout layout = (LinearLayout) getLayoutInflater().inflate(R.layout.post_layout, null);
-                                TextView username_field = (TextView) layout.findViewById(R.id.username);
-                                if(username_field == null) Log.d("test","error.....");
-                                Object username_ = document.getData().get("username");
-                                if(username_ == null) continue;
-                                String username = username_.toString();
-                                username_field.setText(username);
-
-                                TextView bio_field = (TextView) layout.findViewById(R.id.bio);
-                                Object bio_ = document.getData().get("bio");
-                                if(bio_ == null) continue;
-                                String bio = bio_.toString();
-                                bio_field.setText(bio);
-
-                                TextView deadline_field = (TextView) layout.findViewById(R.id.deadline);
-                                Object deadline_ = document.getData().get("deadline");
-                                if(deadline_ == null) continue;
-                                String deadline = deadline_.toString();
-                                deadline_field.setText(deadline);
-
-                                TextView rent_field = (TextView) layout.findViewById(R.id.rent);
-                                Object rent_ = document.getData().get("rent");
-                                if(rent_ == null) continue;
-                                String rent = rent_.toString();
-                                rent_field.setText(rent);
-
-                                TextView address_field = (TextView) layout.findViewById(R.id.address);
-                                Object address_ = document.getData().get("address");
-                                if(address_ == null) continue;
-                                String address = address_.toString();
-                                address_field.setText(address);
-
-                                TextView utilities_field = (TextView) layout.findViewById(R.id.utilities);
-                                Object utilities_ = document.getData().get("utilities");
-                                if(utilities_ == null) continue;
-                                String utilities = utilities_.toString();
-                                utilities_field.setText(utilities);
-
-                                Button btn_accept = (Button) layout.findViewById(R.id.buttonAccept);
-                                btn_accept.setOnClickListener(new View.OnClickListener(){
-                                    @Override
-                                    public void onClick(View view) {
-                                        String id = document.getData().get("id").toString();
-                                        DocumentReference washingtonRef = db.collection("invitation").document(id);
-                                        washingtonRef.update("accept", FieldValue.arrayUnion(myUserId));
-                                    }
-                                });
-
-                                LinearLayout container = (LinearLayout) findViewById(R.id.linearlayout02);
-                                container.addView(layout);
-                                //Log.d("test", document.getId() + " => " + document.getData().get("username"));
+                                // Log.d("test", document.getId() + " => " + document.getData());
+                                String acceptances = document.get("accept").toString();
+                                Log.d(document.getId(), acceptances);
+                                // all_acceptances.put(document.getId(), acceptances);
                             }
                         } else {
                             Log.d("test", "Error getting documents: ", task.getException());
                         }
                     }
                 });
+
+        /*
+        for (Map.Entry<String, String> entry : all_acceptances.entrySet()) {
+            Log.d("test", "here");
+            String key = entry.getKey();
+            String value = entry.getValue();
+
+            TextView user = new TextView(this);
+            user.setText("users: " + value);
+            Button button = new Button(this);
+            button.setText("match!");
+            linearLayout.addView(user);
+            linearLayout.addView(button);
+        }
+         */
     }
-    */
 }
