b4ff00431db97f1fa5ee5267e605c14b9cbe66b5
Winnie Hou <hwinnie@usc.edu>
2022-10-30 18:07:25 -0700

    match buttons configured

--- app/src/main/java/com/example/csci310_final_project/MatchActivity.java
+++ app/src/main/java/com/example/csci310_final_project/MatchActivity.java
@@ -1,22 +1,13 @@
 package com.example.csci310_final_project;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -24,8 +15,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class MatchActivity extends AppCompatActivity {
     int myUserId = 0;
@@ -38,8 +28,6 @@ public class MatchActivity extends AppCompatActivity {
     }
     private void init(){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        // Map<String, String> all_acceptances = new HashMap<String, String>();
-        LinearLayout linearLayout = (LinearLayout) this.findViewById(R.id.linearlayout02);
 
         String userName = "Winnie"; //TODO: get userName of current user
         db.collection("invitation")
@@ -53,27 +41,32 @@ public class MatchActivity extends AppCompatActivity {
                                 // Log.d("test", document.getId() + " => " + document.getData());
                                 String acceptances = document.get("accept").toString();
                                 Log.d(document.getId(), acceptances);
-                                // all_acceptances.put(document.getId(), acceptances);
+                                addMatchButtons(document.getId(), acceptances);
                             }
                         } else {
                             Log.d("test", "Error getting documents: ", task.getException());
                         }
                     }
                 });
+        // TODO: add changes listener
+    }
 
-        /*
-        for (Map.Entry<String, String> entry : all_acceptances.entrySet()) {
-            Log.d("test", "here");
-            String key = entry.getKey();
-            String value = entry.getValue();
-
-            TextView user = new TextView(this);
-            user.setText("users: " + value);
-            Button button = new Button(this);
-            button.setText("match!");
-            linearLayout.addView(user);
-            linearLayout.addView(button);
+    private void addMatchButtons(String key, String value) {
+        LinearLayout linearLayout = (LinearLayout) this.findViewById(R.id.linearlayout02);
+        String[] strings = value.split(", ");
+        if (strings.length > 0) {
+            TextView invitation = new TextView(this);
+            invitation.setText("Invitation: " + key);
+            linearLayout.addView(invitation);
+            for (String string : strings) {
+                TextView user = new TextView(this);
+                user.setText("user " + string);
+                Button button = new Button(this);
+                button.setText("match!");
+                // TODO: add button listener
+                linearLayout.addView(user);
+                linearLayout.addView(button);
+            }
         }
-         */
     }
 }
